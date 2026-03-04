4e76fd6cd03ebf5a9cc6490c207cae5c27e8840d
Animesh <animeshmtb95@gmail.com>
2026-03-04 22:03:29 +0530

    add hotel creation controller

---
+++ src/main/java/AirbnbClone/AirbnbClone/service/HotelService.java
@@ -0,0 +1,17 @@
+package AirbnbClone.AirbnbClone.service;
+
+
+import AirbnbClone.AirbnbClone.dto.HotelDto;
+
+public interface HotelService {
+    HotelDto createNewHotel(HotelDto hotelDto);
+
+    HotelDto getHotelById(Long id);
+
+    HotelDto updateHotelById(Long id, HotelDto hotelDto);
+
+    void deleteHotelById(Long id);
+
+    void activateHotel(Long hotelId);
+
+}
