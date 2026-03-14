22982f88c981e6ce213abd0cfd9fc04de57873d1
Animesh <animeshmtb95@gmail.com>
2026-03-14 17:47:24 +0530

    Implementation of Hotel Booking_V2

--- src/main/java/AirbnbClone/AirbnbClone/repository/BookingRepository.java
+++ src/main/java/AirbnbClone/AirbnbClone/repository/BookingRepository.java
@@ -3,5 +3,5 @@ package AirbnbClone.AirbnbClone.repository;
 import AirbnbClone.AirbnbClone.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface git BookingRepository extends JpaRepository<Booking, Long> {
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
